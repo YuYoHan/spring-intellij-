de4a7bdee1d3fde9d71233f325a0a96d371ed61f
YuYoHan <zxzz8014@naver.com>
2022-11-29 23:27:07 +0900

    [Study] day05

--- ex01/src/main/java/com/example/controller/SampleController.java
+++ ex01/src/main/java/com/example/controller/SampleController.java
@@ -1,8 +1,13 @@
 package com.example.controller;
 
+import com.example.domain.SampleDTO;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
 @RequestMapping("/sample/*")
@@ -20,5 +25,32 @@ public class SampleController {
     public void basic2(int age) {
         log.info("=============basic1===========");
         log.info(age);
+    }
+
+    @GetMapping("basic")
+    public void basicGet() {
+        log.info("============Get방식으로 요청===========");
+    }
+
+    @PostMapping("basic")
+    public void basicPost(int age) {
+        log.info("=============Post방식으로 요청==========");
+        log.info(age);
+    }
+
+    @RequestMapping(value = "basic2", method = {RequestMethod.GET, RequestMethod.POST})
+    public void basic2() {
+        log.info("===========get, post 방식으로 요청=================");
+    }
+
+    // Controller 파라미터 수집 방법
+
+    // DTO 객체로 수집하기
+    @GetMapping("ex01")
+    public String ex01(SampleDTO dto, Model model) {
+        log.info(dto);
+        model.addAttribute("dto", dto);
+        // WEB-INF/views/sample/ex01.jsp
+        return "/sample/ex01";
     }
 }
---
+++ ex01/src/main/webapp/WEB-INF/views/sample/ex01.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 2022-11-29
+  Time: 오후 10:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>ex01</title>
+</head>
+<body>
+    <h1>ex01</h1>
+    <p>
+        Model<br />
+        name : ${dto.name } / age : ${dto.age}
+    </p>
+</body>
+</html>
