a46107afc77b48ec531e75faecaf47a88158cff4
YuYoHan <zxzz8014@naver.com>
2022-12-30 23:44:43 +0900

    [Study] board_getList

---
+++ book_study/src/main/java/com/example/service/BoardServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.service;
+
+import com.example.domain.BoardVO;
+import com.example.mapper.BoardMapper;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Log4j
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService {
+    @Setter(onMethod_ = @Autowired)
+    private BoardMapper mapper;
+
+    @Override
+    public void register(BoardVO board) {
+        log.info("register....." + board);
+        mapper.insertSelectKey(board);
+    }
+
+    @Override
+    public BoardVO get(long boardNum) {
+        return null;
+    }
+
+    @Override
+    public boolean modify(BoardVO board) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(long boardNum) {
+        return false;
+    }
+
+    @Override
+    public List<BoardVO> getList() {
+        return mapper.getList();
+    }
+}
---
+++ book_study/src/test/java/com/example/service/BoardServiceTests.java
@@ -0,0 +1,40 @@
+package com.example.service;
+
+import com.example.domain.BoardVO;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Log4j
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class BoardServiceTests {
+    @Setter(onMethod_ = @Autowired)
+    private BoardService service;
+
+    @Test
+    public void existTest() {
+        log.info(service);
+    }
+
+    @Test
+    public void  resisterTest() {
+        BoardVO board = new BoardVO();
+        board.setBoardTitle("새로 작성한 글");
+        board.setBoardContents("새로 작성한 내용");
+        board.setUserId("user00");
+
+        service.register(board);
+        log.info("생성된 게시글의 번호 : " + board.getBoardNum());
+    }
+
+    @Test
+    public void  testGetList() {
+        service.getList().forEach(board -> log.info(board));
+    }
+
+}
